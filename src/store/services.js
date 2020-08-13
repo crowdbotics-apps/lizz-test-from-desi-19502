38a7d91d9a3e42de6fa6021d4955e1d7e5174dc3
Crowdbotics <team@crowdbotics.com>
2020-08-13 17:00:42 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -3,103 +3,17 @@ const dogAPI = axios.create({
   baseURL: "https://dog.ceo/api/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
-const lizztestfromdesignAPI = axios.create({
-  baseURL: "https://lizz-test-from-desi-19502-prod.herokuapp.com/",
+const dogAPI = axios.create({
+  baseURL: "https://dog.ceo/api/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 function dogapi_get_breeds_image_random_read() {
   return dogAPI.get(`/breeds/image/random`)
 }
-function api_v1_customtext_list() {
-  return lizztestfromdesignAPI.get(`/api/v1/customtext/`)
-}
-function api_v1_customtext_read() {
-  return lizztestfromdesignAPI.get(`/api/v1/customtext/{id}/`)
-}
-function api_v1_customtext_update(requestBody) {
-  return lizztestfromdesignAPI.put(`/api/v1/customtext/{id}/`, requestBody)
-}
-function api_v1_customtext_partial_update(requestBody) {
-  return lizztestfromdesignAPI.patch(`/api/v1/customtext/{id}/`, requestBody)
-}
-function api_v1_homepage_list() {
-  return lizztestfromdesignAPI.get(`/api/v1/homepage/`)
-}
-function api_v1_homepage_read() {
-  return lizztestfromdesignAPI.get(`/api/v1/homepage/{id}/`)
-}
-function api_v1_homepage_update(requestBody) {
-  return lizztestfromdesignAPI.put(`/api/v1/homepage/{id}/`, requestBody)
-}
-function api_v1_homepage_partial_update(requestBody) {
-  return lizztestfromdesignAPI.patch(`/api/v1/homepage/{id}/`, requestBody)
-}
-function api_v1_login_create() {
-  return lizztestfromdesignAPI.post(`/api/v1/login/`)
-}
-function api_v1_signup_create(requestBody) {
-  return lizztestfromdesignAPI.post(`/api/v1/signup/`, requestBody)
-}
-function rest_auth_login_create(requestBody) {
-  return lizztestfromdesignAPI.post(`/rest-auth/login/`, requestBody)
-}
-function rest_auth_logout_list() {
-  return lizztestfromdesignAPI.get(`/rest-auth/logout/`)
-}
-function rest_auth_logout_create() {
-  return lizztestfromdesignAPI.post(`/rest-auth/logout/`)
-}
-function rest_auth_password_change_create(requestBody) {
-  return lizztestfromdesignAPI.post(`/rest-auth/password/change/`, requestBody)
-}
-function rest_auth_password_reset_create(requestBody) {
-  return lizztestfromdesignAPI.post(`/rest-auth/password/reset/`, requestBody)
-}
-function rest_auth_password_reset_confirm_create(requestBody) {
-  return lizztestfromdesignAPI.post(
-    `/rest-auth/password/reset/confirm/`,
-    requestBody
-  )
-}
-function rest_auth_registration_create(requestBody) {
-  return lizztestfromdesignAPI.post(`/rest-auth/registration/`, requestBody)
-}
-function rest_auth_registration_verify_email_create(requestBody) {
-  return lizztestfromdesignAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    requestBody
-  )
-}
-function rest_auth_user_read() {
-  return lizztestfromdesignAPI.get(`/rest-auth/user/`)
-}
-function rest_auth_user_update(requestBody) {
-  return lizztestfromdesignAPI.put(`/rest-auth/user/`, requestBody)
-}
-function rest_auth_user_partial_update(requestBody) {
-  return lizztestfromdesignAPI.patch(`/rest-auth/user/`, requestBody)
+function dogapi_get_breeds_image_random_read() {
+  return dogAPI.get(`/breeds/image/random`)
 }
 export const apiService = {
   dogapi_get_breeds_image_random_read,
-  api_v1_customtext_list,
-  api_v1_customtext_read,
-  api_v1_customtext_update,
-  api_v1_customtext_partial_update,
-  api_v1_homepage_list,
-  api_v1_homepage_read,
-  api_v1_homepage_update,
-  api_v1_homepage_partial_update,
-  api_v1_login_create,
-  api_v1_signup_create,
-  rest_auth_login_create,
-  rest_auth_logout_list,
-  rest_auth_logout_create,
-  rest_auth_password_change_create,
-  rest_auth_password_reset_create,
-  rest_auth_password_reset_confirm_create,
-  rest_auth_registration_create,
-  rest_auth_registration_verify_email_create,
-  rest_auth_user_read,
-  rest_auth_user_update,
-  rest_auth_user_partial_update
+  dogapi_get_breeds_image_random_read
 }
